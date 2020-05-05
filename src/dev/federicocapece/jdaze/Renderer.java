15efbd063a5fbae5cae8d14a902c850dc6360c74
Fede Capece <capece.1744999@studenti.uniroma1.it>
2020-05-05 22:00:47 +0200

    javadoc Renderer

--- src/dev/federicocapece/jdaze/Renderer.java
+++ src/dev/federicocapece/jdaze/Renderer.java
@@ -6,26 +6,62 @@ import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
 
-//TODO: Probably the renderer class can be protected, test it.
-//if it is possible to set it protected consider renaming it to Engine.canvas
-class Renderer extends Canvas {
-    //TODO: javadoc
 
-    //canvas
+/**
+ * The Engine Renderer, this is basically a Canvas managed directly from the Engine.
+ * Add Engine.renderer to your GUI, then call Engine.start() to make the renderer start drawing.
+ */
+class Renderer extends Canvas {
+    /**
+     * The graphics used to draw on canvas,
+     * this should never be used except from inside the Engine.
+     */
     private Graphics canvasGraphics;
 
+
+    /**
+     * Half of the canvas height, this is useful for offset calculations
+     */
     private int halfCanvasHeight;
+
+
+    /**
+     * Half of the canvas width, this is useful for offset calculations
+     */
     private int halfCanvasWidth;
 
-    //buffered image
+
+    /**
+     * The buffered image that this renderer uses for drawing
+     */
     private BufferedImage image;
+
+
+    /**
+     * The graphic used to draw on the BufferedImage used by this rendered.
+     */
     private Graphics2D bufferGraphics;
 
-    //manual pixel writings to buffered image
-    private int[] pixels;
+    /**
+     * The pixels of the BufferedImage used by this rendered.
+     * You could use those to do cool stuff (like filters and effects).
+     */
+    public int[] pixels;
 
-    protected static Camera camera = new Camera();;
+    /**
+     * The camera used by the renderer
+     * for now it cannot be changed.
+     * I'm not sure if i will change this and allow multiple cameras
+     * in future versions.
+     */
+    protected final static Camera camera = new Camera();;
 
+
+    /**
+     * Initialize the renderer.
+     * This should be called every time the canvas size is changed.
+     * It is automatically called when the Engine starts.
+     */
     public void init(){
         //canvas init
         canvasGraphics = getGraphics();
@@ -43,21 +79,32 @@ class Renderer extends Canvas {
         clean();
     }
 
-    public void update(GameObject gameObject) {
+    /**
+     * Draw a GameObject into the buffer if it is inside the screen.
+     * @param gameObject the GameObject to be drawn
+     */
+    protected void update(GameObject gameObject) {
         //TODO: add checks to avoid drawing if the offset is out of the camera
         int cameraOffsetX = (int)((gameObject.position.x - camera.position.x)* camera.getScale()) + halfCanvasWidth;
         int cameraOffsetY = (int)((gameObject.position.y - camera.position.y)* camera.getScale()) + halfCanvasHeight;
         gameObject.draw(bufferGraphics,cameraOffsetX , cameraOffsetY, camera.getScale());
     }
 
-    public void update() {
+    /**
+     * Draw the buffer onto the canvas
+     */
+    protected void update() {
         canvasGraphics.drawImage(image,0,0,null);
     }
 
-    public void clean() {
-        //turn the image full white
+
+    /**
+     * Turn the screen buffer full white
+     */
+    protected void clean() {
         for (int i = 0; i < pixels.length; i++) {
             pixels[i] = 0xffffff;
         }
     }
+
 }
