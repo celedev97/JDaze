ecf35d2b31eeb09f60037d99b6b4b8b696c0b8c8
Fede Capece <capece.1744999@studenti.uniroma1.it>
2020-05-05 22:28:20 +0200

    added javadoc to Circlecollider, plus little change in the code

--- src/dev/federicocapece/jdaze/collider/CircleCollider.java
+++ src/dev/federicocapece/jdaze/collider/CircleCollider.java
@@ -3,34 +3,53 @@ package dev.federicocapece.jdaze.collider;
 import dev.federicocapece.jdaze.GameObject;
 import dev.federicocapece.jdaze.Vector;
 
+/**
+ * The most basic collider implementation: a circle.
+ */
 public class CircleCollider extends Collider {
-    //TODO: javadoc
 
+    /**
+     * The ray of the circle
+     */
     private float ray;
 
+    /**
+     * Create a circle collider for a GameObject
+     * @param gameObject the GameObject that will be linked to this collider
+     * @param ray the ray of the circle
+     */
     public CircleCollider(GameObject gameObject, float ray) {
         super(gameObject);
         this.ray = ray;
     }
 
+
+    /**
+     * Return the size of this collider
+     * @return the ray of the circle
+     */
     @Override
     public float size() {
         return ray;
     }
 
+    /**
+     * Check if this collider collide with another collider
+     * @param collider
+     * @param firstTry
+     * @return
+     */
     @Override
     public boolean collide(Collider collider, boolean firstTry){
-        if(collider.getClass() == this.getClass()){
-            CircleCollider circle2 = (CircleCollider) collider;
-            return (circle2.ray + this.ray)< Vector.distance(this.gameObject.position, collider.gameObject.position);
-        }
-
+        /*
+         * this collider automatically delegate the collision check to the other one since it's a basic collider
+         * and doesn't consider the existence of other colliders
+         */
         if(firstTry)
             return collider.collide(this, false);
 
-        //both the colliders don't have a way to check the collision
-        System.out.println(this.getClass()+" has no way to check collision with: "+collider.getClass());
-        return false;
+        //the other collider doesn't have a way to check collision with a CircleCollider, so i'm just gonna use it like a circle
+        return (collider.size() + this.ray)< Vector.distance(this.gameObject.position, collider.gameObject.position);
     }
 
 
